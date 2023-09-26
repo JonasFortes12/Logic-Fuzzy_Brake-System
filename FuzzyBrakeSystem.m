42e3ebd294dba534e53a48e1d6b82493f60e277d
JonasFortes12 <jonascforte@hotmail.com>
2023-09-26 20:21:23 -0300

    fix: rules functions

--- FuzzyBrakeSystem.m
+++ FuzzyBrakeSystem.m
@@ -1,8 +1,22 @@
 
+%______________________ Conjuntos Nebulosos ____________________________
+
 % Conjunto nebuloso da pressão no pedal de freio 
 trimPressureBrake = [[0,0,50],[30,50,70],[50,100,100]];
 
 % Conjunto nebuloso de velocidades(Carro e Rodas)
 trimVelocity = [[0,0,60],[20,50,80],[40,100,100]];
 
-disp(pertinence(55, trimVelocity(1,:)));
+
+
+
+
+disp(rule01(pertinence(80,trimPressureBrake(1,1:3))));
+
+
+
+
+
+
+
+
